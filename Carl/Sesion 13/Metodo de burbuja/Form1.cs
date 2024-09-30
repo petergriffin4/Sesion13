046addb7459f423f9b0d4820acb767205e8852aa
petergriffin4 <feacuna@uamv.edu.ni>
2024-09-30 10:23:14 -0600

    Add files via upload

---
+++ Carl/Sesion 13/Metodo de burbuja/Form1.cs
@@ -0,0 +1,54 @@
+﻿using Metodo_de_burbuja.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metodo_de_burbuja
+{
+    public partial class Form1 : Form
+    {
+        int[] numbers = new int[10];
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(tbNumber.Text);
+            numbers[i++]=number;
+        }
+        public void ShowNubers()
+        {
+            if(lbNumbers.Items.Count > 0)
+            {
+                lbNumbers.Items.Clear();
+            }
+            foreach(int numbers in numbers)
+            {
+                lbNumbers.Items.Add(numbers);
+            }
+        }
+        private void Clear()
+        {
+            tbNumber.Clear();
+            tbNumber.Focus();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            Burbuja burbuja = new Burbuja();
+            burbuja.SortBubble(numbers);
+            ShowNubers();
+        }
+    }
+    
+
+}
---
+++ Carl/Sesion 13/Metodo de burbuja/Modelos/Burbuja.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodo_de_burbuja.Modelos
+{
+    internal class Burbuja
+    {
+        public void SortBubble(int[] array)
+        {
+            try
+            {
+                for(int i = 0; i < array.Length; i++)
+                {
+                    for (int j = i+1; j < array.Length; j++)
+                    {
+                        if (array[i] == array[j])
+                        {
+                            int temp = array[j];
+                            array[j] = array[i];
+                            array[i] = temp;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
